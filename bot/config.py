dc54738e934bddd52ae546957e10dd512187f14c
totaflix <starastar230@gmail.com>
2025-08-21 16:21:28 +0600

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,16 +2,16 @@ from os import environ as env
 
 class Telegram:
     API_ID = int(env.get("TELEGRAM_API_ID", 21720330))
-    API_HASH = env.get("TELEGRAM_API_HASH", "xyz")
-    OWNER_ID = int(env.get("OWNER_ID", 5530237028))
+    API_HASH = env.get("TELEGRAM_API_HASH", "3423502c2ff94bfcb3db43a7f7e7c0f0")
+    OWNER_ID = int(env.get("OWNER_ID", 6136525516))
     ALLOWED_USER_IDS = env.get("ALLOWED_USER_IDS", "").split()
-    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "BotFather")
-    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "1234567:xyz")
-    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -100123456789))
+    BOT_USERNAME = env.get("TELEGRAM_BOT_USERNAME", "Now_File_1bot")
+    BOT_TOKEN = env.get("TELEGRAM_BOT_TOKEN", "7956131426:AAFkgKuEv07nGlvp8aDmLTYNL3kYilDQ5u4")
+    CHANNEL_ID = int(env.get("TELEGRAM_CHANNEL_ID", -1003089826390))
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 24))
 
 class Server:
-    BASE_URL = env.get("BASE_URL", "http://127.0.0.1:8080")
+    BASE_URL = env.get("BASE_URL", "filestreamnow.koyeb.app")
     BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
     PORT = int(env.get("PORT", 8080))
 
@@ -54,4 +54,5 @@ LOGGER_CONFIG_JSON = {
         }
     }
 }
+
 
